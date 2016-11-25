cb084dea40c75842d7649a21442f4aca48bfddc6
xseignard <xavier.siegnard@gmail.com>
2016-11-25 12:36:15 +0100

    BLE to OSC bridge OK

--- bridge/main.js
+++ bridge/main.js
@@ -4,11 +4,16 @@ const osc = require('node-osc');
 const client = new osc.Client('127.0.0.1', 12345);
 
 const ids = {
-	uuid: 'c89694e52602',
+	raquettes: [
+		{ name: 'r1', uuid: 'c89694e52602' },
+		{ name: 'r2', uuid: 'c6e22bfb9c7a' },
+	],
 	serviceUUID: '6e400001b5a3f393e0a9e50e24dcca9e',
 	rxCharacteristic: '6e400003b5a3f393e0a9e50e24dcca9e',
 	txCharacteristic: '6e400002b5a3f393e0a9e50e24dcca9e',
 }
+
+let i = 0;
 
 const sendMessage = (raquette, value) => {
 	client.send(`/${raquette}`, value);
@@ -21,17 +26,20 @@ noble.on('stateChange', (state) => {
 
 noble.on('discover', (peripheral) => {
 	console.log(peripheral.uuid);
-	if (peripheral.uuid === ids.uuid) {
-		console.log(`uuid: ${peripheral.uuid} // local name: ${peripheral.advertisement.localName}`);
+	if (ids.raquettes.find((raquette) => { return raquette.uuid === peripheral.uuid })) {
 		noble.stopScanning();
 		peripheral.connect(() => {
-			console.log('connected!');
+			console.log(`${peripheral.advertisement.localName} connecté`);
+			i++;
+			if (i === 2) noble.stopScanning();
+			else noble.startScanning();
 			peripheral.discoverServices([], (error, services) => {
 				const uartService = services.filter((service) => { return service.uuid === ids.serviceUUID })[0];
 				uartService.discoverCharacteristics([], (error, chars) => {
 					const rxCharacteristic = chars.filter((char) => { return char.uuid === ids.rxCharacteristic })[0];
 					rxCharacteristic.on('read', (state) => {
 						const value = parseInt(state.toString());
+						console.log(`${peripheral.advertisement.localName} : ${value}`);
 						if (value > 500) {
 							sendMessage(peripheral.advertisement.localName, value);
 						}
